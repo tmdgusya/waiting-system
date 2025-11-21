9d248e2a9dc8c37c7389b37b28c23d7debaf0896
tmdgusya <dev0jsh@gmail.com>
2025-11-21 22:14:16 +0900

    Add redis_queue :

---
+++ queue/redis_queue.go
@@ -0,0 +1,32 @@
+package queue
+
+import "github.com/redis/go-redis/v9"
+
+type RedisQueue struct {
+	rdb       *redis.Client
+	euqueueFn *redis.Script
+}
+
+func NewRedisQueue(rdb *redis.Client) *RedisQueue {
+	scripts := redis.NewScript(`
+		-- KEYS[1] = queue:seq
+		-- KEYS[2] = queue:z
+		-- KEYS[3] = queue:userSeq
+		-- ARGV[1] = userKey
+
+		local existing = redis.call('HGET', KEYS[3], ARGV[1])
+		if existing then
+			return existing
+		end
+
+		local seq = redis.call("INCR", KEYS[1])
+		redis.call('ZADD', KEYS[2], seq, ARGV[1])
+		redis.call('HSET', KEYS[3], ARGV[1], seq)
+		return seq
+	`)
+
+	return &RedisQueue{
+		rdb:       rdb,
+		euqueueFn: scripts,
+	}
+}
