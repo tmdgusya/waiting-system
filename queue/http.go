520bd078815fd0ec3c0dfff1783e3d1b4f378dd1
tmdgusya <dev0jsh@gmail.com>
2025-11-22 14:27:34 +0900

    1 Phase

--- queue/http.go
+++ queue/http.go
@@ -1,6 +1,9 @@
 package queue
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type HTTPHandler struct {
 	q *RedisQueue
@@ -12,10 +15,39 @@ func NewHTTPHandler(q *RedisQueue) *HTTPHandler {
 	}
 }
 
+func getOrSetUserKey(w http.ResponseWriter, r *http.Request) string {
+	const cookieName = "queue_user"
+	c, err := r.Cookie(cookieName)
+	if err == nil && c.Value != "" {
+		return c.Value
+	}
+	// 새 발급
+	key := newToken()
+	http.SetCookie(w, &http.Cookie{
+		Name:     cookieName,
+		Value:    key,
+		Path:     "/",
+		HttpOnly: true,
+	})
+	return key
+}
+
 func (h *HTTPHandler) JoinQueue(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	userKey := getOrSetUserKey(w, r)
+	userKey := getOrSetUserKey(w, r) // 쿠키 기반
 
 	res, err := h.q.Join(ctx, userKey)
+	if err != nil {
+		http.Error(w, "queue error", http.StatusInternalServerError)
+		return
+	}
+
+	resp := map[string]interface{}{
+		"position":   res.Position,
+		"etaSeconds": res.ETASeconds,
+		"userKey":    userKey,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(resp)
 }
--- queue/redis_queue.go
+++ queue/redis_queue.go
@@ -3,7 +3,10 @@ package queue
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -61,6 +64,15 @@ func NewRedisQueue(rdb *redis.Client) *RedisQueue {
 	}
 }
 
+func (q *RedisQueue) getThroughputPerSecond(ctx context.Context) (int64, error) {
+	val, err := q.rdb.HGet(ctx, "queue:config", "throughput_per_sec").Result()
+	if err != nil {
+		return 0, err
+	}
+
+	return toInt64(val)
+}
+
 func (q *RedisQueue) Join(ctx context.Context, userKey string) (*JoinResult, error) {
 	rawSeq, err := q.euqueueFn.Run(ctx, q.rdb, []string{
 		keySeq, keyZ, keyUserSeq,
@@ -74,5 +86,65 @@ func (q *RedisQueue) Join(ctx context.Context, userKey string) (*JoinResult, err
 
 	if err != nil {
 		return nil, err
+	}
+
+	// 포지션을 계산하는 함수
+	pos, err := q.rdb.ZCount(ctx, keyZ, "-inf", fmt.Sprintf("%d", seq)).Result()
+
+	if err != nil {
+		return nil, err
+	}
+
+	throughput, err := q.getThroughputPerSecond(ctx)
+	if err != nil || throughput <= 0 {
+		throughput = 50
+	}
+	etaSec := int64(pos) / throughput
+
+	return &JoinResult{
+		Seq:        seq,
+		Position:   pos,
+		ETASeconds: etaSec,
+	}, nil
+}
+
+func newToken() string {
+	return uuid.New().String()
+}
+
+func (q *RedisQueue) adminOnce(ctx context.Context) {
+	throughput, err := q.getThroughputPerSecond(ctx)
+	if err != nil || throughput <= 0 {
+		throughput = 50
+	}
+
+	users, err := q.rdb.ZRange(ctx, keyZ, 0, throughput-1).Result()
+	if err != nil || len(users) == 0 {
+		return
+	}
+
+	pipe := q.rdb.Pipeline()
+	for _, user := range users {
+		token := newToken()
+		pipe.Set(ctx, "queue:admin:token:"+token, user, 10*time.Minute)
+		pipe.ZRem(ctx, keyZ, user)
+	}
+	_, err = pipe.Exec(ctx)
+	if err != nil {
+		log.Printf("failed to execute pipeline: %v", err)
+	}
+}
+
+func (q *RedisQueue) RunAdmissionWorker(ctx context.Context) {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			q.adminOnce(ctx)
+		}
 	}
 }
